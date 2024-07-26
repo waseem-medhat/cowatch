bafff6daf654cd964b8951b8c0b06994181dadd1
waseem-medhat <wipdev.tech@gmail.com>
2024-07-26 14:38:44 +0300

    Scan & handle each line before printing to stdout

--- cmd/cowatch/main.go
+++ cmd/cowatch/main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"sync"
+
+	codes "github.com/avearmin/stylecodes"
 )
 
 func main() {
-	wg := sync.WaitGroup{}
-
+	colors := []string{codes.ColorGreen, codes.ColorBlue}
 	sites := []string{"google.com", "github.com"}
-	wg.Add(len(sites))
 
-	for _, site := range sites {
-		go func(site string) {
-			cmd := exec.Command("ping", site)
+	wg := &sync.WaitGroup{}
 
-			cmd.Stdout = os.Stdout
-			err := cmd.Run()
-			if err != nil {
-				log.Fatal(err)
-			}
+	for i, site := range sites {
+		wg.Add(1)
+		go func(site string, color string) {
+			run(site, color)
 			wg.Done()
-		}(site)
+		}(site, colors[i])
 	}
 
 	wg.Wait()
+}
+
+func run(site string, color string) {
+	cmd := exec.Command("ping", site)
+	stdout, err := cmd.StdoutPipe()
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Fprintln(os.Stdout, color, site, line, codes.ResetColor)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/waseem-medhat/cowatch
 
 go 1.22.3
+
+require github.com/avearmin/stylecodes v1.0.1 // indirect
