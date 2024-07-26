a9cde50fafe0edcfcbc7f456b661d6dd62de7507
waseem-medhat <wipdev.tech@gmail.com>
2024-07-26 14:58:27 +0300

    Handle SIGINT

--- cmd/cowatch/main.go
+++ cmd/cowatch/main.go
@@ -6,29 +6,39 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	codes "github.com/avearmin/stylecodes"
 )
 
-func main() {
-	colors := []string{codes.ColorGreen, codes.ColorBlue}
-	sites := []string{"google.com", "github.com"}
+var colors = []string{
+	codes.ColorGreen,
+	codes.ColorBlue,
+	codes.ColorYellow,
+	codes.ColorCyan,
+	codes.ColorMagenta,
+}
 
+func main() {
+	sites := []string{"google.com", "github.com", "duck.com"}
 	wg := &sync.WaitGroup{}
+	sigintChan := make(chan os.Signal, 1)
+	signal.Notify(sigintChan, os.Interrupt)
 
 	for i, site := range sites {
 		wg.Add(1)
 		go func(site string, color string) {
-			run(site, color)
+			run(site, color, sigintChan)
 			wg.Done()
-		}(site, colors[i])
+		}(site, colors[i%len(colors)])
 	}
 
 	wg.Wait()
 }
 
-func run(site string, color string) {
+func run(site string, color string, sigintChan chan os.Signal) {
 	cmd := exec.Command("ping", site)
 	stdout, err := cmd.StdoutPipe()
 
@@ -40,8 +50,15 @@ func run(site string, color string) {
 	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Fprintln(os.Stdout, color, site, line, codes.ResetColor)
+		fmt.Fprintf(os.Stdout, "%v[%v]: %v%v\n", color, site, line, codes.ResetColor)
 	}
+
+	go func() {
+		<-sigintChan
+		if err := cmd.Process.Signal(syscall.SIGINT); err != nil {
+			fmt.Println(err)
+		}
+	}()
 
 	err = cmd.Wait()
 	if err != nil {
