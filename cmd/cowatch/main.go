d5f37945229a875f86e0593318ed55b3eeb8e1b2
waseem-medhat <wipdev.tech@gmail.com>
2024-07-26 14:23:13 +0300

    Add initial concurrent running of commands

--- cmd/cowatch/main.go
+++ cmd/cowatch/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+)
 
 func main() {
-	fmt.Println("hello world")
+	wg := sync.WaitGroup{}
+
+	sites := []string{"google.com", "github.com"}
+	wg.Add(len(sites))
+
+	for _, site := range sites {
+		go func(site string) {
+			cmd := exec.Command("ping", site)
+
+			cmd.Stdout = os.Stdout
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+			wg.Done()
+		}(site)
+	}
+
+	wg.Wait()
 }
